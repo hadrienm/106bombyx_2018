5bd9e40db33b8e3f44e2d3d0798416453d0b18dd
root <root@localhost.localdomain>
2019-02-20 06:48:05 -0500

    all options works but does round

--- 106bombyx.c
+++ 106bombyx.c
@@ -21,6 +21,29 @@ void launchk(char **av)
 	}
 }
 
+void launchi0i1(char **av)
+{
+	float k = 1.00;
+	int n = atoi(av[1]);
+	int i0 = atoi(av[2]);
+	int i1 = atoi(av[3]);
+	float resultat = (float)n;
+	if (i1 - i0 <= 0)exit(84);
+	
+	for (;k <= 4.00;)
+	{
+		resultat = (float)n;
+		for (int a = 0; a != i0 - 1; a++)
+			resultat = (k * (resultat)) * ((1000 - (resultat)) / 1000);
+		printf("%0.2f %0.2f\n", k, resultat);
+		for (int q = 0; i0 + q != i1; q++) {
+			resultat = (k * resultat) * ((1000 - (resultat)) / 1000);
+			printf("%0.2f %0.2f\n", k, resultat);
+		}
+		k += 0.01;
+	}
+}
+
 int main(int ac, char **av)
 {
 	if (ac != 3 && ac != 4)return 84;
@@ -28,6 +51,6 @@ int main(int ac, char **av)
 		for (int j = 0; av[i][j] != '\0'; ++j)
 			if (av[i][j] > 57 || av[i][j] < 42)return 84;
 	if (ac == 3)launchk(av);
-	//else launchi0i1(av);
+	else launchi0i1(av);
 	return 0;
 }
